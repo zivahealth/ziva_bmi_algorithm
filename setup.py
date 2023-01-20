29c9e5424f7631e5b743244e80a59d53ae19e54e
joshegladwell <joshegladwell@gmail.com>
2023-01-20 13:30:58 -0500

    Initialize git repo

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='An algorithm that takes a user's height, weight, age, sex at birth, and waist circumference to return a baseline health score',
+    author='Josh Gladwell',
+    license='MIT',
+)
