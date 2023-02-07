1bc11e3861bb74d55559801e27737f8d2b1175ad
joshegladwell <joshegladwell@gmail.com>
2023-02-07 14:28:45 -0500

    Fix bug

--- src/data/03-generate_scoring_data.py
+++ src/data/03-generate_scoring_data.py
@@ -5,20 +5,20 @@ import numpy as np
 import pandas as pd
 
 # Define body fat percentage ranges
-bfp_ranges_f_twenties = [0, 9.5, 14, 16.4, 19.5, 22.8, 27.2, 58.6, 1]
-bfp_ranges_f_thirties = [0, 9.5, 14, 17.5, 20.9, 24.7, 29.2, 58.6, 1]
-bfp_ranges_f_forties = [0, 9.5, 14, 19.9, 23.9, 27.7, 32, 58.6, 1]
-bfp_ranges_f_fifties = [0, 9.5, 14, 22.6, 27.1, 30.5, 34.6, 58.6, 1]
-bfp_ranges_f_sixties = [0, 9.5, 14, 23.3, 28, 31.4, 35.5, 58.6, 1]
+bfp_ranges_f_twenties = [0, 9.5, 14, 16.4, 19.5, 22.8, 27.2, 58.6, 100]
+bfp_ranges_f_thirties = [0, 9.5, 14, 17.5, 20.9, 24.7, 29.2, 58.6, 100]
+bfp_ranges_f_forties = [0, 9.5, 14, 19.9, 23.9, 27.7, 32, 58.6, 100]
+bfp_ranges_f_fifties = [0, 9.5, 14, 22.6, 27.1, 30.5, 34.6, 58.6, 100]
+bfp_ranges_f_sixties = [0, 9.5, 14, 23.3, 28, 31.4, 35.5, 58.6, 100]
 
 # Save female ranges
 bfp_ranges_f = [bfp_ranges_f_twenties, bfp_ranges_f_thirties, bfp_ranges_f_forties, bfp_ranges_f_fifties, bfp_ranges_f_sixties]
 
-bfp_ranges_m_twenties = [0, 4, 8, 10.6, 14.9, 18.7, 23.2, 58.6, 1]
-bfp_ranges_m_thirties = [0, 4, 8, 14.6, 18.3, 21.4, 25, 58.6, 1]
-bfp_ranges_m_forties = [0, 4, 8, 17.5, 20.7, 23.5, 26.7, 58.6, 1]
-bfp_ranges_m_fifties = [0, 4, 8, 19.2, 22.2, 24.7, 27.9, 58.6, 1]
-bfp_ranges_m_sixties = [0, 4, 8, 19.8, 22.7, 25.3, 28.5, 58.6, 1]
+bfp_ranges_m_twenties = [0, 4, 8, 10.6, 14.9, 18.7, 23.2, 58.6, 100]
+bfp_ranges_m_thirties = [0, 4, 8, 14.6, 18.3, 21.4, 25, 58.6, 100]
+bfp_ranges_m_forties = [0, 4, 8, 17.5, 20.7, 23.5, 26.7, 58.6, 100]
+bfp_ranges_m_fifties = [0, 4, 8, 19.2, 22.2, 24.7, 27.9, 58.6, 100]
+bfp_ranges_m_sixties = [0, 4, 8, 19.8, 22.7, 25.3, 28.5, 58.6, 100]
 
 # Save male ranges
 bfp_ranges_m = [bfp_ranges_m_twenties, bfp_ranges_m_thirties, bfp_ranges_m_forties, bfp_ranges_m_fifties, bfp_ranges_m_sixties]
